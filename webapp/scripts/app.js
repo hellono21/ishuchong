6ef02d25459b1e4001282f881e837ed8ce2aa17c
hellono21 <hellono21@gmail.com>
2013-10-12 17:02:46 +0800

    yeoman-angular & mean develop env set up

---
+++ webapp/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('ishuchongApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
